d7788e54fce7e07b576ef303714681874550d2f5
Andrew Bruce <abruce1711@gmail.com>
2020-05-02 22:03:00 +0100

    Got decreasing temperature proving

--- src/reactor.adb
+++ src/reactor.adb
@@ -3,23 +3,39 @@ is
    procedure removeControlRod is
    begin
       currentRods := currentRods-1;
-      currentTemperatureIncrease := currentTemperatureIncrease+1;
+      currentReactorPower := currentReactorPower+1;
    end removeControlRod;
 
    procedure addControlRod is
    begin
       currentRods := currentRods+1;
-      currentTemperatureIncrease := currentTemperatureIncrease-1;
+      currentReactorPower := currentReactorPower-1;
    end addControlRod;
 
    procedure increaseTemperature (temp : out Integer) is
       begin
-      temp := currentTemperature + currentTemperatureIncrease;
+      temp := currentTemperature + currentReactorPower;
       if temp > 100 then
          currentTemperature := 100;
       else
          currentTemperature := temp;
       end if;
-
    end increaseTemperature;
+
+   procedure decreaseTemperature (temp : out Integer) is
+   begin
+      temp := 0;
+      if currentWaterSupply >= 10 then
+         temp := currentTemperature - currentWaterSupply/10;
+         if temp < 0 then
+            currentTemperature := 0;
+         else
+            currentTemperature := temp;
+         end if;
+      else
+         currentTemperature := currentTemperature-1;
+      end if;
+
+   end decreaseTemperature;
+
 end Reactor;
--- src/reactor.ads
+++ src/reactor.ads
@@ -3,32 +3,37 @@ is
    subtype ControlRods is Integer range 0..5;
    subtype WaterSupply is Integer range 0..100;
    subtype Temperature is Integer range 0..100;
-   subtype TemperatureIncrease is Integer range 0..5;
+   subtype ReactorPower is Integer range 0..5;
    subtype Electricity is Integer range 0..1000;
 
    currentRods : ControlRods := ControlRods'Last;
    currentWaterSupply : WaterSupply := WaterSupply'Last;
    currentTemperature : Temperature := Temperature'First;
-   currentTemperatureIncrease : TemperatureIncrease := TemperatureIncrease'First;
+   currentReactorPower : ReactorPower := ReactorPower'First;
    currentElectricityProduced : Electricity := Electricity'First;
 
    function ReactorCheck return Boolean is
       (currentTemperature < 100 and currentWaterSupply > 0);
 
    procedure removeControlRod with
-     Global => (In_Out => (currentRods, currentTemperatureIncrease), Proof_In => (currentTemperature, currentWaterSupply)),
-     Pre => currentRods > 0 and currentTemperatureIncrease < 5 and ReactorCheck,
+     Global => (In_Out => (currentRods, currentReactorPower), Proof_In => (currentTemperature, currentWaterSupply)),
+     Pre => currentRods > 0 and currentReactorPower < 5 and ReactorCheck,
      Post => currentRods < currentRods+1 and ReactorCheck;
 
    procedure addControlRod with
-     Global => (In_Out => (currentRods, currentTemperatureIncrease)),
-     Pre => currentRods < 5 and currentTemperatureIncrease > 0,
+     Global => (In_Out => (currentRods, currentReactorPower)),
+     Pre => currentRods < 5 and currentReactorPower > 0,
      Post => currentRods > currentRods-1;
 
    procedure increaseTemperature (temp : out Integer) with
-     Global => (In_Out => currentTemperature, Input => currentTemperatureIncrease),
+     Global => (In_Out => currentTemperature, Input => currentReactorPower),
      Pre => currentTemperature < 100,
-     Post => currentTemperature >= currentTemperature-1;
+     Post => currentTemperature > currentTemperature-1;
+
+   procedure decreaseTemperature (temp : out Integer) with
+     Global => (In_Out => currentTemperature, Input => currentWaterSupply),
+     Pre => currentTemperature > 0,
+     Post => currentTemperature < currentTemperature+1;
 
 
 end Reactor;
